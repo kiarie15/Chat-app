79170aa3651a8a209f0679a019fb291f92a06a06
kiarie15 <justinkiarie0@gmail.com>
2025-02-02 18:34:32 +0300

    Sunday feb 2

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,13 @@
 import express from "express"
+import {login,logout,signup} from "../controllers/auth.controllers.js";
 
-const router = express.Router()
+const router = express.Router();
 
-export default router; 
+router.post("/signup",signup);
+
+router.post("/login", login);
+
+router.post("/logout",logout);
+
+
+export default router;  
